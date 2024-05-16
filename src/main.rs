a51db14a2563149ebb4132c63af4e2a0d083c30f
zcfabra <christianwgnam@gmail.com>
2024-05-15 23:24:51 -0400

    some python stuff

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,20 @@ pub mod tokenizer;
 use tokenizer::Tokenizer;
 
 fn main() {
-    let src = String::from("(10 != 10)");
+    let src =
+        String::from(
+        "if x := 90:
+            t: int = x + 10
+            t += 10
+            vals = [x for x in range(t + 10)]
+        ");
     let tokens =
         Tokenizer::new(src)
         .tokenize()
         .expect("Failed to tokenize");
-    println!("{:?}", tokens);
+    for tok in tokens{
+        println!("{:?}", tok);
+    }
     // let ast = Parser::new(tokens).parse();
     // let result = Evaluator::new(ast).eval();
 
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -6,17 +6,44 @@ pub struct Tokenizer {
 #[derive(Debug)]
 pub enum TokenType {
     Add,
+    AddEq,
     Sub,
+    SubEq,
     Mul,
+    MulEq,
     Div,
-    Int,
-    Identifier,
-    LParen,
-    RParen,
-    Bang,
+    DivEq,
     Eq,
     NotEq,
+    Or,
+    And,
+
+    Int,
+    Identifier,
+
+    LParen,
+    RParen,
+    LSquare,
+    RSquare,
+    LBrace,
+    RBrace,
+
+    Arrow,
+
+    Bang,
     Assignment,
+    Pipe,
+    Bar,
+    For,
+    Return,
+    Def,
+    Walrus,
+    Colon,
+    Indent,
+    Newline,
+    If,
+    In, 
+    Range,
 }
 #[derive(Debug)]
 pub enum TokenizerError {
@@ -36,18 +63,76 @@ impl Tokenizer {
         while self.r < self.src.len() {
             let ch = self.src[self.r];
             match ch {
-                ' ' => {self.r += 1; self.l = self.r},
-                '+' => tokens.push(self.get_char_op(ch, TokenType::Add)),
-                '-' => tokens.push(self.get_char_op(ch, TokenType::Sub)),
-                '*' => tokens.push(self.get_char_op(ch, TokenType::Mul)),
-                '/' => tokens.push(self.get_char_op(ch, TokenType::Div)),
+                ' ' => {
+                    self.r += 1;
+                    self.l = self.r
+                }
+                // Indent + Newlines
+                '\n' => tokens.push(self.get_char_op(ch, TokenType::Newline)),
+                '\t' => tokens.push(self.get_char_op(ch, TokenType::Indent)),
+                // Single Char Operators
                 '(' => tokens.push(self.get_char_op(ch, TokenType::LParen)),
                 ')' => tokens.push(self.get_char_op(ch, TokenType::RParen)),
+                '[' => tokens.push(self.get_char_op(ch, TokenType::LSquare)),
+                ']' => tokens.push(self.get_char_op(ch, TokenType::RSquare)),
+                '{' => tokens.push(self.get_char_op(ch, TokenType::RSquare)),
+                '}' => tokens.push(self.get_char_op(ch, TokenType::RSquare)),
+
+                '+' => {
+                     let token = if self.next_char_is('=') {
+                        self.get_long_op(TokenType::AddEq)
+                    } else {
+                        self.get_char_op(ch, TokenType::Add)
+                    };
+                    tokens.push(token);
+                },
+                '-' => {
+                     let token = if self.next_char_is('=') {
+                        self.get_long_op(TokenType::SubEq)
+                    } else if self.next_char_is('>') {
+                        self.get_long_op(TokenType::Arrow)
+                    }else {
+                        self.get_char_op(ch, TokenType::Sub)
+                    };
+                    tokens.push(token);
+                },
+                '*' => {
+                     let token = if self.next_char_is('=') {
+                        self.get_long_op(TokenType::MulEq)
+                    } else {
+                        self.get_char_op(ch, TokenType::Mul)
+                    };
+                    tokens.push(token);
+                },
+                '/' => {
+                     let token = if self.next_char_is('=') {
+                        self.get_long_op(TokenType::DivEq)
+                    } else {
+                        self.get_char_op(ch, TokenType::Div)
+                    };
+                    tokens.push(token);
+                },
                 '=' => {
                     let token = if self.next_char_is('=') {
                         self.get_long_op(TokenType::Eq)
                     } else {
                         self.get_char_op(ch, TokenType::Assignment)
+                    };
+                    tokens.push(token);
+                }
+                ':' => {
+                    let token = if self.next_char_is('=') {
+                        self.get_long_op(TokenType::Walrus)
+                    } else {
+                        self.get_char_op(ch, TokenType::Colon)
+                    };
+                    tokens.push(token);
+                }
+                '|' => {
+                    let token = if self.next_char_is('>') {
+                        self.get_long_op(TokenType::Pipe)
+                    } else {
+                        self.get_char_op(ch, TokenType::Bar)
                     };
                     tokens.push(token);
                 }
@@ -62,6 +147,9 @@ impl Tokenizer {
                 '0'..='9' => {
                     tokens.push(self.get_numerical_literal());
                 }
+                'a'..='z' | 'A'..='Z' | '_' => {
+                    tokens.push(self.get_alpha_literal());
+                }
                 _ => {
                     println!("{:?}", ch);
                     return Err(TokenizerError::InvalidChar);
@@ -74,6 +162,36 @@ impl Tokenizer {
         return self.r + 1 < self.src.len() && self.src[self.r + 1] == ch;
     }
 
+    pub fn get_alpha_literal(&mut self) -> Token {
+        while self.r < self.src.len() 
+            && (
+                ('a'..='z').contains(&self.src[self.r])
+                || ('A'..='Z').contains(&self.src[self.r])
+                || '_' == self.src[self.r]
+            )
+        {
+            self.r += 1;
+        }
+        let literal = self.src[self.l..self.r].iter().collect();
+        self.l = self.r;
+        let token_type =
+            Tokenizer::get_keyword(&literal).unwrap_or(TokenType::Identifier);
+        return Token::new(literal, token_type);
+    }
+    pub fn get_keyword(literal: &String) -> Option<TokenType>{
+        let literal_str= literal.as_str();
+        return match literal_str {
+            "for" => Some(TokenType::For),
+            "def" => Some(TokenType::Def),
+            "or" => Some(TokenType::Or),
+            "and" => Some(TokenType::And),
+            "if" => Some(TokenType::If),
+            "in" => Some(TokenType::In),
+            "range" => Some(TokenType::Range),
+            "return" => Some(TokenType::Return),
+            _ => None
+        };
+    }
     pub fn get_numerical_literal(&mut self) -> Token {
         while self.r < self.src.len() && ('0'..='9').contains(&self.src[self.r]) {
             self.r += 1;
